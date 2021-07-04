6a40bbedc2c599a71dee4398a2ad450feef40657
Eduardo <educastro100@gmail.com>
2021-07-04 20:52:55 -0300

    Ajustes nas rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 const routes: Routes = [
   {path: "listar" , component: ListarComponent},
-  {path: "cadastrar" , component: CadastrarComponent}
+  {path: "cadastrar" , component: CadastrarComponent},
+  {path: "", redirectTo: "/listar" , pathMatch: "full"},
 ];
 
 @NgModule({
